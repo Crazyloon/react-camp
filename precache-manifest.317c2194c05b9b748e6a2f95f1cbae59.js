7fdd4bf239be1666d259d943787a36f023a601ae
v_rdd611 <russell.j.dow@avanade.com>
2020-07-21 18:57:32 -0700

    Updates

---
+++ precache-manifest.317c2194c05b9b748e6a2f95f1cbae59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fbc3089a87359c523b93935e806047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50553b01d4ce6f13ac87",
+    "url": "/static/css/main.543b388f.chunk.css"
+  },
+  {
+    "revision": "56745fdf4f1d184510d4",
+    "url": "/static/js/2.e33ccb51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e33ccb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50553b01d4ce6f13ac87",
+    "url": "/static/js/main.23533b32.chunk.js"
+  },
+  {
+    "revision": "685df32d37089c28c38a",
+    "url": "/static/js/runtime-main.8f49f7f4.js"
+  }
+]);
