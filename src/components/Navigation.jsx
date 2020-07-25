0c591e35926953a9d3a3f1522727aea6b8c336e2
v_rdd611 <russell.j.dow@avanade.com>
2020-07-25 13:52:25 -0700

    Refactor Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faBars, faFilm, faTh } from '@fortawesome/free-solid-svg-icons'
 
 export const Navigation = ({currentLesson, onSetLesson}) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const setLesson = (i) => {
+    onSetLesson(i);
+    setMenuOpen(false);
+  }
+
   return (
     <header className="navigation">
       <nav className="navbar">
-        <ul className="nav-list">
-          <li className={(currentLesson === 0 ? "nav-item-current " : "") + "nav-item"} onClick={() => onSetLesson(0)}>
-            <button className="nav-btn" href="#">Home</button>
+        <ul className={"nav-list" + (menuOpen ? " responsive" : "")}>
+          <li className={(currentLesson === 0 ? "active " : "") + "nav-item-brand"} onClick={() => setLesson(0)}>
+            <FontAwesomeIcon icon={faHome} size={'2x'} />
+            <a href="#" className="nav-link nav-brand-name">Home</a>
           </li>
-          <li className={(currentLesson === 1 ? "nav-item-current " : "") + "nav-item"} onClick={() => onSetLesson(1)}>
-            <button className="nav-btn" href="#">Lesson 1</button>
+          <li className={(currentLesson === 1 ? "active " : "") + "nav-item"} onClick={() => setLesson(1)}>
+            <FontAwesomeIcon icon={faFilm} size={'2x'} />
+            <a href="#" className="nav-link">Lesson 1</a>
           </li>
-          <li className={(currentLesson === 2 ? "nav-item-current " : "") + "nav-item"} onClick={() => onSetLesson(2)}>
-            <button className="nav-btn" href="#">Lesson 2</button>
+          <li className={(currentLesson === 2 ? "active " : "") + "nav-item"} onClick={() => setLesson(2)}>
+            <FontAwesomeIcon icon={faTh} size={'2x'} />
+            <a href="#" className="nav-link">Lesson 2</a>
+          </li>
+          <li className={"nav-item nav-toggle icon"} onClick={() => setMenuOpen(!menuOpen)}>
+              <FontAwesomeIcon icon={faBars} size={'2x'} />
           </li>
         </ul>
       </nav>
